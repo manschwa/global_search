e312bf2d9d83938ffa4f8561aa91c1c9de7791d8
Manuel Schwarz <manschwa@uos.de>
2016-09-20 10:25:04 +0200

    Fixes pagination CSS.

--- views/show/_pagination.php
+++ views/show/_pagination.php
@@ -2,9 +2,7 @@
     <div class='pagination'>
         <hr>
         <? foreach ($search->getPages(Request::get('page')) as $page): ?>
-            <a href='<?= URLHelper::getLink('', array('search' => $search->query, 'page' => $page)) ?>' class='<?= Request::get('page') == $page ? 'current' : ''?>'>
-                <?= $page + 1 ?>
-            </a> 
+            <a href='<?= URLHelper::getLink('', array('search' => $search->query, 'page' => $page)) ?>' class='<?= Request::get('page') == $page ? 'current' : ''?>'><?= $page + 1 ?></a>
         <? endforeach; ?>
     </div>
 <? endif; ?>
