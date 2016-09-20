b3e89ade85fb50281fced906c3ac02fbcdd68bb6
Manuel Schwarz <manschwa@uos.de>
2016-09-20 17:27:04 +0200

    Improving pagination: double arrows to the first/last result page.

--- views/show/_pagination.php
+++ views/show/_pagination.php
@@ -1,7 +1,11 @@
 <? if ($search && $search->countResultPages() > 1): ?>
     <div class='pagination'>
         <hr>
-        <? // left arrow for the previous page ?>
+        <? // left arrow for the _first_ page ?>
+        <? if ((Request::get('page') - ($search->getPagesShown() / 2)) > 0): ?>
+            <a href='<?= URLHelper::getLink('', array('search' => $search->query, 'page' => 0)) ?>'> <?= Icon::create('arr_2left') ?></a>
+        <? endif; ?>
+        <? // left arrow for the _previous_ page ?>
         <? if ((Request::get('page') - ($search->getPagesShown() / 2)) > 0): ?>
             <a href='<?= URLHelper::getLink('', array('search' => $search->query, 'page' => (Request::get('page') - 1))) ?>'> <?= Icon::create('arr_1left') ?></a>
         <? endif; ?>
@@ -10,9 +14,13 @@
             <a href='<?= URLHelper::getLink('', array('search' => $search->query, 'page' => $page)) ?>' class='<?= Request::get('page') == $page ? 'current' : ''?>'><?= $page + 1 ?></a>
         <? endforeach; ?>
 
-        <? // right arrow for the next page ?>
+        <? // right arrow for the _next_ page ?>
         <? if ((Request::get('page') + ($search->getPagesShown() / 2)) < ($search->countResultPages() - 1)): ?>
             <a href='<?= URLHelper::getLink('', array('search' => $search->query, 'page' => (Request::get('page') + 1))) ?>'> <?= Icon::create('arr_1right') ?></a>
+        <? endif; ?>
+        <? // right arrow for the _last_ page ?>
+        <? if ((Request::get('page') + ($search->getPagesShown() / 2)) < ($search->countResultPages() - 1)): ?>
+            <a href='<?= URLHelper::getLink('', array('search' => $search->query, 'page' => ($search->countResultPages() - 1))) ?>'> <?= Icon::create('arr_2right') ?></a>
         <? endif; ?>
     </div>
 <? endif; ?>
